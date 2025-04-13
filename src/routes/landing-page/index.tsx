a7e18212fd5d72098c4c109350c00feb245d6c7f
heyjiawei <jaewei.j.w@gmail.com>
2025-04-14 00:41:54 +0800

    feat: landing page

---
+++ src/routes/landing-page/index.tsx
@@ -0,0 +1,78 @@
+import { Button, Modal, message } from "antd";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { attendeeCreate } from "../../api";
+import RequestEmailForm from "./form.component";
+
+const successMessages = {
+  title_registered: "Successfully Registered",
+  check_your_email: "Check your email!",
+};
+
+export default function Page() {
+  const [open, setOpen] = useState(false);
+  const [openSuccessPopup, setOpenSuccessPopup] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const errorNotification = (content: string) => {
+    messageApi.open({
+      type: "error",
+      content,
+    });
+  };
+
+  const mutation = useMutation({
+    mutationFn: attendeeCreate,
+    onSuccess: () => {
+      setOpen(false);
+      setOpenSuccessPopup(true);
+      mutation.reset();
+    },
+    onError: (error, variables, context) => {
+      // TODO: Error thrown by attendeeCreate not triggering onError callback
+      console.log(`mutation.onError error: ${error}`);
+      console.log(`mutation.onError variables: ${variables}`);
+      console.log(`mutation.onError context: ${context}`);
+      errorNotification(error.message);
+    },
+  });
+
+  const onSubmitForm = (formData) => {
+    mutation.mutate(formData);
+  };
+
+  console.log(`mutation status: ${mutation.status}`);
+  return (
+    <>
+      {contextHolder}
+      <Button type="primary" onClick={() => setOpen(true)}>
+        Request an invite
+      </Button>
+      <Modal
+        open={open}
+        onCancel={() => setOpen(false)}
+        footer={null}
+        destroyOnClose
+      >
+        <h1>Request an Invite</h1>
+        <RequestEmailForm
+          onSubmit={onSubmitForm}
+          isPending={mutation.isPending}
+        />
+      </Modal>
+      <Modal
+        title={successMessages.title_registered}
+        open={openSuccessPopup}
+        onOk={() => setOpenSuccessPopup(false)}
+        onCancel={() => setOpenSuccessPopup(false)}
+        footer={(_, { OkBtn }) => (
+          <>
+            <OkBtn />
+          </>
+        )}
+      >
+        {successMessages.check_your_email}
+      </Modal>
+    </>
+  );
+}

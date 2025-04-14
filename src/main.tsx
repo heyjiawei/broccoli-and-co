879c7d960fc0b5509c1cf9400b2ed314af5e9f9b
heyjiawei <jaewei.j.w@gmail.com>
2025-04-14 23:52:27 +0800

    fix: layout css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import LandingPage from "./routes/landing-page";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
--- src/routes/landing-page/index.tsx
+++ src/routes/landing-page/index.tsx
@@ -3,10 +3,25 @@ import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { attendeeCreate } from "../../api";
 import RequestEmailForm from "./form.component";
+import StickyHeader from "../../components/sticky-header/sticky-header.component";
+import StickyFooter from "../../components/sticky-footer/sticky-footer.component";
+import classes from "./layout.module.css";
 
 const successMessages = {
   title_registered: "Successfully Registered",
   check_your_email: "Check your email!",
+};
+
+const headerContent = {
+  company_logo: "Broccoli and Co",
+};
+
+const pageContent = {
+  eat_healthy_live_better: "Eat Healthy. Live Better.",
+};
+
+const footerContent = {
+  company_rights: "© [2025] Broccoli & Co. All rights reserved.",
 };
 
 export default function Page() {
@@ -37,38 +52,50 @@ export default function Page() {
     mutation.mutate(formData);
   };
 
-  console.log(`mutation status: ${mutation.status}`);
   return (
     <>
       {contextHolder}
-      <Button type="primary" onClick={() => setOpen(true)}>
-        Request an invite
-      </Button>
-      <Modal
-        open={open}
-        onCancel={() => setOpen(false)}
-        footer={null}
-        destroyOnClose
-      >
-        <h1>Request an Invite</h1>
-        <RequestEmailForm
-          onSubmit={onSubmitForm}
-          isPending={mutation.isPending}
-        />
-      </Modal>
-      <Modal
-        title={successMessages.title_registered}
-        open={openSuccessPopup}
-        onOk={() => setOpenSuccessPopup(false)}
-        onCancel={() => setOpenSuccessPopup(false)}
-        footer={(_, { OkBtn }) => (
-          <>
-            <OkBtn />
-          </>
-        )}
-      >
-        {successMessages.check_your_email}
-      </Modal>
+      <StickyHeader>
+        <h1>{headerContent.company_logo}</h1>
+      </StickyHeader>
+
+      <section className={`${classes.screenHeight} ${classes.container}`}>
+        <h1 className={classes.typographLarge}>
+          {pageContent.eat_healthy_live_better}
+        </h1>
+        <Button type="primary" onClick={() => setOpen(true)}>
+          Request an invite
+        </Button>
+        <Modal
+          open={open}
+          onCancel={() => setOpen(false)}
+          footer={null}
+          destroyOnClose
+        >
+          <h1>Request an Invite</h1>
+          <RequestEmailForm
+            onSubmit={onSubmitForm}
+            isPending={mutation.isPending}
+          />
+        </Modal>
+        <Modal
+          title={successMessages.title_registered}
+          open={openSuccessPopup}
+          onOk={() => setOpenSuccessPopup(false)}
+          onCancel={() => setOpenSuccessPopup(false)}
+          footer={(_, { OkBtn }) => (
+            <>
+              <OkBtn />
+            </>
+          )}
+        >
+          {successMessages.check_your_email}
+        </Modal>
+      </section>
+
+      <StickyFooter>
+        <p>{footerContent.company_rights}</p>
+      </StickyFooter>
     </>
   );
 }

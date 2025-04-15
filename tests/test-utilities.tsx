29a1e6d79f7ddf2ce116562340184f990ba3c880
heyjiawei <jaewei.j.w@gmail.com>
2025-04-16 00:48:48 +0800

    fix: typescript error about toBeInTheDocument

--- tests/test-utilities.tsx
+++ tests/test-utilities.tsx
@@ -1,8 +1,9 @@
 import { render } from "@testing-library/react";
 import { Alert } from "antd";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import type { ReactElement } from "react";
 
-export const renderWithProvider = (ui) => {
+export const renderWithProvider = (ui: ReactElement) => {
   const queryClient = new QueryClient();
   const { ErrorBoundary } = Alert;
   return render(
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "./tests/setup.js",
+    setupFiles: "./tests/setup.ts",
   },
 } as UserConfig);

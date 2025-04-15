cc83a9ce92cf1771b4027d5a058049a4215cad5d
heyjiawei <jaewei.j.w@gmail.com>
2025-04-15 23:47:37 +0800

    refactor: form translations

--- src/routes/landing-page/form.component.tsx
+++ src/routes/landing-page/form.component.tsx
@@ -1,22 +1,8 @@
 import { Form, Input } from "antd";
 import { MailOutlined, UserOutlined } from "@ant-design/icons";
+import { content } from "./translations";
 import SubmitButton from "../../components/submit-button.component";
 import type { AttendeeCreateBody } from "../../api/types";
-
-const formFields = {
-  name: "My Full Name",
-  email: "My E-mail",
-  confirm_email: "Confirm E-mail",
-  send: "Send Invite",
-};
-
-const validationMessages = {
-  confirm_email: "Please confirm your email!",
-};
-
-const errorMessages = {
-  email_not_match: "The new email that you entered do not match!",
-};
 
 export default function RequestEmailForm({
   onSubmit,
@@ -39,7 +25,7 @@ export default function RequestEmailForm({
       requiredMark={false}
     >
       <Form.Item
-        label={formFields.name}
+        label={content.form.name}
         name="name"
         validateTrigger="onBlur"
         rules={[
@@ -53,28 +39,30 @@ export default function RequestEmailForm({
         <Input prefix={<UserOutlined />} placeholder="Alex Anderson" />
       </Form.Item>
       <Form.Item
-        label={formFields.email}
+        label={content.form.email}
         name="email"
         rules={[{ type: "email" }, { required: true }]}
       >
         <Input prefix={<MailOutlined />} placeholder="alex.anderson@mail.com" />
       </Form.Item>
       <Form.Item
-        label={formFields.confirm_email}
+        label={content.form.confirm_email}
         name="confirm"
         dependencies={["email"]}
         hasFeedback
         rules={[
           {
             required: true,
-            message: validationMessages.confirm_email,
+            message: content.validation.confirm_email,
           },
           ({ getFieldValue }) => ({
             validator(_, value) {
               if (!value || getFieldValue("email") === value) {
                 return Promise.resolve();
               }
-              return Promise.reject(new Error(errorMessages.email_not_match));
+              return Promise.reject(
+                new Error(content.messages.email_not_match)
+              );
             },
           }),
         ]}
@@ -83,7 +71,7 @@ export default function RequestEmailForm({
       </Form.Item>
       <Form.Item>
         <SubmitButton form={form} isPending={isPending}>
-          {formFields.send}
+          {content.form.send}
         </SubmitButton>
       </Form.Item>
     </Form>
--- src/routes/landing-page/translations.ts
+++ src/routes/landing-page/translations.ts
@@ -12,5 +12,15 @@ export const content = {
   messages: {
     title_registered: "Successfully Registered",
     check_your_email: "Check your email!",
+    email_not_match: "The new email that you entered do not match!",
+  },
+  form: {
+    name: "My Full Name",
+    email: "My E-mail",
+    confirm_email: "Confirm E-mail",
+    send: "Send Invite",
+  },
+  validation: {
+    confirm_email: "Please confirm your email!",
   },
 };
